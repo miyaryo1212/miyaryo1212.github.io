95170b47e28139e0b868b45f7d55cd5a26232511
miyaryo1212 <miyaryo1212@gmail.com>
2022-02-24 01:15:39 +0900

    [Update] Site

---
+++ Footer_3.js
@@ -0,0 +1,11 @@
+document.write(`<footer>
+
+<!-- 内容 START -->
+<p>Copyright(C)miyaryo1212</p>
+<!-- 内容 END -->
+
+<!-- 下の行を変更したり削除すると、無料テンプレートの利用規約違反となります。 -->
+<p><a href="http://japanism.info/free-template.html" target="_blank" rel="nofollow">Free Template</a></p>
+<!-- 上の行を変更したり削除すると、無料テンプレートの利用規約違反となります。 -->
+
+</footer>`)
---
+++ header_3.js
@@ -0,0 +1,18 @@
+document.write(`<header>
+<div id="header_block">
+    <div id="site_title">
+        <a href="../../index.html">miyaryo1212</a>
+    </div>
+    <div id="main_menu">
+        <nav>
+            <ul class="list_design">
+                <li><a href="../../index.html">Home</a></li>
+                <li><a href="../../skills.html">Skills</a></li>
+                <li><a href="../../works.html">Works</a></li>
+                <li><a href="../../Games.html">Games</a></li>
+                <li><a href="../../index.html#SNS">SNS</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+</header>`);
