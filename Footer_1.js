b9ee76293937450de7f94ec7b2709622719fa939
miyaryo1212 <83214456+miyaryo1212@users.noreply.github.com>
2022-04-02 02:27:47 +0900

    Update

--- Footer_1.js
+++
@@ -1,7 +0,0 @@
-document.write(`<footer>
-
-<p>Copyright(C)miyaryo1212</p>
-
-<p><a href="http://japanism.info/free-template.html" target="_blank" rel="nofollow">Free Template</a></p>
-
-</footer>`)
